9c956af586d5e375b31898d5ed8179044330c7c7
CoD <caravanofdeath@icloud.com>
2022-09-26 13:17:25 +0100

    First soak test

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,71 @@
-// use embedded_hal::delay::blocking::DelayUs;
-
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
 use esp_idf_hal::ledc::*;
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
+use std::sync::Arc;
+use std::sync::Mutex;
 
+type Channel<'a> = Arc<Mutex<LedcDriver<'a, CHANNEL0, LedcTimerDriver<'a, TIMER0>>>>;
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
-
-    println!("Configuring output channel");
+    println!("Configuring output channel GPIO 1");
 
     let peripherals = Peripherals::take().unwrap();
-    let config = config::TimerConfig::new().frequency(2.kHz().into());
-    let mut channel = LedcDriver::new(
+    let config = config::TimerConfig::new().frequency(1000.into());
+    let channel: Channel = Arc::new(Mutex::new(LedcDriver::new(
         peripherals.ledc.channel0,
         LedcTimerDriver::new(peripherals.ledc.timer0, &config)?,
-        peripherals.pins.gpio4,
+        peripherals.pins.gpio1,
         &config,
-    )?;
+    )?));
+    println!("Configuring input channel GPIO 0 (active low)");
+    let mut sense = PinDriver::input(peripherals.pins.gpio0)?;
+    sense.set_pull(Pull::Up)?;
 
-    println!("Starting duty-cycle loop");
-
-    let max_duty = channel.get_max_duty();
-    for numerator in (1..=10).into_iter().cycle() {
-        println!("Duty {}/10", numerator);
-        channel.set_duty(max_duty * numerator / 10)?;
-        FreeRtos::delay_ms(2000);
-    }
-
+    println!("Starting contactor control loop");
+    let mut state = false;
     loop {
+        if sense.is_low() && !state {
+            println!("Debounce 100ms started");
+            FreeRtos::delay_ms(100);
+            if sense.is_low() {
+                activate_contactor(channel.clone())?;
+                state = true;
+            }
+
+            continue;
+        } else if sense.is_low() && state {
+            println!(
+                "State: {state} Output:{:?}/256",
+                channel.clone().lock().unwrap().get_duty()
+            );
+        } else {
+            deactivate_contactor(channel.clone())?;
+            state = false;
+        }
         FreeRtos::delay_ms(1000);
     }
+}
+
+fn deactivate_contactor(channel_a: Channel) -> anyhow::Result<()> {
+    if let Ok(mut channel) = channel_a.lock() {
+        if channel.disable().is_ok() {
+            println!("Contactor disabled");
+        } else {
+            println!("Contactor disable fault");
+        }
+    }
+    Ok(())
+}
+
+fn activate_contactor(channel_a: Channel) -> Result<(), anyhow::Error> {
+    if let Ok(mut channel) = channel_a.lock() {
+        let max_duty = channel.get_max_duty();
+        channel.set_duty(max_duty)?;
+        channel.enable()?;
+        println!("Set duty 100%");
+        FreeRtos::delay_ms(200);
+        return Ok(channel.set_duty(1)?);
+    };
+    Err(anyhow::anyhow!("Failed to get mutex"))
 }
